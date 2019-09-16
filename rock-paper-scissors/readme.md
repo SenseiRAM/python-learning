0ca5e05159c2202a794cb8459226769af3c68b92
SenseiRAM <richard.a.mitchell@gmail.com>
2019-09-16 13:02:30 -0500

    Built simple rock, paper, scissors game.

---
+++ rock-paper-scissors/readme.md
@@ -0,0 +1,6 @@
+# Rock, Paper, Scissors
+A vey simple Rock, Paper, Scissors game. Made for Talk Python's [100 Days of Code course][https://training.talkpython.fm/courses/explore_100days_in_python/100-days-of-code-in-python].
+
+The player inputs their name and then selects rock, paper, or scissors and sees how they do against the computer. There are while loops in place to ensure that inputs are correct, and to see if the player wishes to play again. I also imported the time module to add a little dramatic pause between throws and make the game more interesting. It's not as fun when the computer just spits out all the results in a big blob of text. Much more fun to have a little anticipation between button presses and the results!
+
+I guess technically there's even a little recursion in here, as the game loop calls itself if you choose to play again.
---
+++ rock-paper-scissors/rps.py
@@ -0,0 +1,124 @@
+import random
+import time
+
+class Roll:
+    def __init__(self, name, defeats, defeated_by):
+        self.name = name
+        self.defeats = defeats
+        self.defeated_by = defeated_by
+
+    def can_defeat(self, roll): # TODO Add tie condition, can't just be true/false
+        if roll.name == self.defeats:
+            return True
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+
+
+def print_header():
+    print("-----------------------------")
+    print("        Welcome to           ")
+    print(" ROCK, PAPER, SCISSORS!!!!!! ")
+
+
+def build_rolls():
+    rock = Roll("Rock", "Scissors", "Paper")
+    paper = Roll("Paper", "Rock", "Scissors")
+    scissors = Roll("Scissors", "Paper", "Rock")
+
+    return [rock, paper, scissors]
+
+
+def get_player_name():
+    return input("What is your name, player? ")
+
+
+def get_player_roll(rolls, player):
+    roll = None
+    while roll not in ["r", "p", "s"]:
+        roll = input(f'\nDo you choose [R]ock, [P]aper, or [S]cissors, {player.name}? ').lower()
+        print("")
+    if roll == "r":
+        return rolls[0]
+    elif roll == "p":
+        return rolls[1]
+    else:
+        return rolls[2]
+
+def dramatic_wait(secs):
+
+    time.sleep(secs)
+
+def main():
+    print_header()
+
+    rolls = build_rolls()
+
+    name = get_player_name()
+
+    player1 = Player(name)
+    player2 = Player("Computer")
+
+
+
+    game_loop(player1, player2, rolls)
+
+
+def game_loop(player1, player2, rolls):
+    count = 0
+    player_score = 0
+    cpu_score = 0
+    answer = None
+
+    while count < 3:
+        p2_roll = random.choice(rolls)
+        p1_roll = get_player_roll(rolls, player1)
+
+        outcome = p1_roll.can_defeat(p2_roll)
+
+        print(f"{player1.name} throws {p1_roll.name}!")
+        dramatic_wait(1)
+        print(f"{player2.name} throws {p2_roll.name}!")
+        dramatic_wait(1)
+        # display winner for this round
+
+        if p2_roll == p1_roll:
+            print("It's a tie!")
+            dramatic_wait(1)
+
+        elif outcome:
+            print(f"{p1_roll.name} beats {p2_roll.name}!")
+            dramatic_wait(1)
+            print(f"{player1.name} wins!!!")
+            dramatic_wait(1)
+            player_score += 1
+
+        else:
+            print(f"{p2_roll.name} beats {p1_roll.name}!")
+            dramatic_wait(1)
+            print(f"{player2.name} wins :(")
+            dramatic_wait(1)
+            cpu_score += 1
+
+        count += 1
+
+    # Compute who won
+    if player_score > cpu_score:
+        print(f'\nCongratulations, {player1.name}, you have {player_score} points and you win!')
+    elif player_score == cpu_score:
+        print(f'\n{player1.name} and {player2.name} have the same score, it\'s a tie!')
+    else:
+        print(f"\nSorry {player1.name}, but {player2.name} has {cpu_score} points and you lose.")
+
+    while answer not in ['y','n']:
+        answer = input('Would you like to play again? [y]es or [n]o?').lower()
+        if answer == 'y':
+            game_loop(player1, player2, rolls)
+        elif answer == 'n':
+            print("Good game! See you next time!")
+            break
+
+if __name__ == "__main__":
+    main()
