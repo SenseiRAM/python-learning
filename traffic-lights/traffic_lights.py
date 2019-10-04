ff85d442d4b7adedb05530724fbdac9ec2f68eaa
SenseiRAM <richard.a.mitchell@gmail.com>
2019-10-04 09:06:54 -0500

    Refactored. Turned main script into function, implemented if name == main statement. Now abides by style guide.

--- traffic-lights/traffic_lights.py
+++ traffic-lights/traffic_lights.py
@@ -6,9 +6,19 @@ import random
 light_colors = ['Green', 'Yellow', 'Red']
 light_iter = itertools.cycle(light_colors)
 
-while True:
-   sys.stdout.write('\r      ')
-   sys.stdout.flush()
-   sys.stdout.write('\r' + next(light_iter))
-   sys.stdout.flush()
-   time.sleep(random.randrange(3,11))
+
+def traffic_lights():
+    while True:
+        sys.stdout.write('\r      ')
+        sys.stdout.flush()
+        sys.stdout.write('\r' + next(light_iter))
+        sys.stdout.flush()
+        time.sleep(random.randrange(3, 11))
+
+
+def main():
+    traffic_lights()
+
+
+if __name__ == '__main__':
+    main()
