a7b8bfcd99f52315354df9a3a77c3a84e2ec9192
SenseiRAM <richard.a.mitchell@gmail.com>
2019-10-21 09:09:26 -0500

    Revert "Created barebones RSS feed parser."
    This reverts commit 6815e9a44ab2cbd278d244741c1ca8d4600a42d2.

--- rss_parse/pull_xml.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-URL = "https://www.metacritic.com/rss/games/switch.xml"
-
-if __name__ == '__main__':
-    r = requests.get(URL)
-    with open('metacritic_switch.xml', 'wb') as f:
-        f.write(r.content)
