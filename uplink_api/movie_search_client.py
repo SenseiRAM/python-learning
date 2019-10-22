92b758cf4b164f29100f1505d59c205e87997894
SenseiRAM <richard.a.mitchell@gmail.com>
2019-10-22 09:17:19 -0500

    Finished basic application.

--- uplink_api/movie_search_client.py
+++ uplink_api/movie_search_client.py
@@ -1,7 +1,17 @@
-
+import requests
 import uplink
 
-class MovieClient(uplink.Consumer):
+
+@uplink.json
+class MovieSearchClient(uplink.Consumer):
 
     def __init__(self):
-        pass
+        super().__init__(base_url="http://movie_service.talkpython.fm/")
+
+    @uplink.get("/api/search/{keyword}")
+    def movies_by_keyword(self, keyword) -> requests.models.Response:
+        """ Gets movies by keywords """
+
+    @uplink.get("/api/director/{director_name}")
+    def movies_by_director(self, director_name) -> requests.models.Response:
+        """ Gets movies by director """
--- uplink_api/program.py
+++ uplink_api/program.py
@@ -1,9 +1,53 @@
-import uplink
-import movie_search_client
+from movie_search_client import MovieSearchClient
 
 
 def main():
-    print('woo')
+    val = 'RUN'
+
+    while val:
+        print('Welcome to Movie Search!')
+        val = input('Would you like to search by [k]eyword or [d]irector? (or [q]uit? ').lower()
+
+        if val == 'k':
+            search_by_keyword()
+        elif val == 'd':
+            search_by_director()
+        elif val == 'q':
+            val = None
+
+
+def search_by_keyword():
+    keyword = input('Please enter a keyword to search for: ').lower()
+
+    svc = MovieSearchClient()
+    response = svc.movies_by_keyword(keyword)
+    response.raise_for_status()
+    movies = response.json()['hits']
+
+    for idx, m in enumerate(movies, 1):
+        print(f"{idx}. {m.get('title')}")
+
+    if not movies:
+        print('No results found...')
+
+    print()
+
+
+def search_by_director():
+    director = input("Please enter a director's last name to search for: ").lower()
+
+    svc = MovieSearchClient()
+    response = svc.movies_by_director(director)
+    response.raise_for_status()
+    movies = response.json()['hits']
+
+    for idx, m in enumerate(movies, 1):
+        print(f"{idx}. {m.get('title')}")
+
+    if not movies:
+        print('No results found...')
+
+    print()
 
 
 if __name__ == '__main__':
